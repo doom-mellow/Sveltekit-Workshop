a5b144160023f4971848cc2ef1caa900543f54d6
doom-mellow <mustafa.mahearsh@gmail.com git config --global user.name>
2024-08-26 23:10:44 +0530

    initial

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default {
+  plugins: [sveltekit()],
+};
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
